cb00aa98e5fc2a5e6af83d1bea578372ffbc8d07
didiks2-b <didik.s2@gmail.com>
2026-08-20 18:24:18 +0700

    Update config.h
    aktifkan led di GP29

--- keyboards/0_didik/87tkl/vial/config.h
+++ keyboards/0_didik/87tkl/vial/config.h
@@ -2,5 +2,8 @@
 
 // Vial
 #define VIAL_KEYBOARD_UID {0x8B, 0x9B, 0xB5, 0xD3, 0x69, 0xB0, 0x27, 0x05}
-#define VIAL_UNLOCK_COMBO_ROWS { 5, 5 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 20 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
+
+#define LED_CAPS_LOCK_PIN GP29 // Ganti B2 dengan pin mikrokontroler Anda
+#define LED_PIN_ON_STATE 1   // 1 jika HIGH aktif, 0 jika LOW aktif
